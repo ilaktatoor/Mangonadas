44709b30feffff79e74f6829362c9c3b09ccb727
ilaktatoor <cemmnuelaxa@gmail.com>
2024-06-21 19:52:56 -0600

    readme

--- target/debug/cproducto.d
+++ target/debug/cproducto.d
@@ -1,1 +1,1 @@
-/home/rootatkali/Documents/Programming/Rust/cproducto/target/debug/cproducto: /home/rootatkali/Documents/Programming/Rust/cproducto/src/main.rs
+/home/rootatkali/Documents/Programming/Rust/Mangonadas/target/debug/cproducto: /home/rootatkali/Documents/Programming/Rust/Mangonadas/src/main.rs
